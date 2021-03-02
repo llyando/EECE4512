efee75615ada231c3ab6decc644fcf96d022f85e
llyando <llyando@users.noreply.github.com>
2021-03-02 11:16:29 -0500

    Data plotted. Needs to be currated. Otherwise lab is looking ok.

--- Lab02.m
+++ Lab02.m
@@ -4,23 +4,20 @@ plotSpacing = 2;
 
 % Create labels for subplots
 labels = [ ...
-"1000 Hz 30 sec. ACC working direction",...
-"100 Hz 30 sec. ACC working direction", ...
-"10 Hz 30 sec. ACC working direction",  ...
-"1000 Hz 60 sec. Stepping Up and Down", ...
-"100 Hz 60 sec. Stepping Up and Down",  ...
-"1000 Hz 30 sec. Stepping Jeans Up and Down", ...
-"100 Hz 30 sec. Stepping Jeans Up and Down",  ...
-"1000 Hz 60 sec. Walking",              ...
-"100 Hz 60 sec. Walking",               ...
-"1000 Hz 60 sec. Walking Jeans",        ...
-"100 Hz 60 sec. Walking Jeans"          ...
+"1000 Hz 30 sec. Bicep Flex",...
+"1000 Hz 30 sec. Bicep Heavy", ...
+"1000 Hz 30 sec. Bicep Resting",  ...
+"1000 Hz 30 sec. Face Anger", ...
+"1000 Hz 30 sec. Face Smile",  ...
+"1000 Hz 30 sec. Tricep Flex", ...
+"1000 Hz 30 sec. Tricep Heavy",  ...
+"1000 Hz 30 sec. Tricep Resting"
 ];
 
 % Data source folder
 dataFolder = 'LAB02/'; 
 % Pull all text files from source folder
-fileNameList = ls('LAB02/*/*.txt');
+fileNameList = ls('LAB02/*.txt');
 % Get number of files for looping and parsing
 numOfFiles = size(fileNameList,1);
 
@@ -61,64 +58,15 @@ end
 
 sgtitle('Raw Data vs Time');
 
-% Plot in terms of g using roughly estimated calibration values from data
-% 
-
-figure(2)
-
-for k = 1:numOfFiles
-   
-   % These data points come from the LAB01/Data Calibration Recordings
-   % files.
-   CMin = 406;
-   CMax = 618;
-   
-   % Equation comes from the ACC data sheet using best fit from rotating
-   %    along the Z-axis per the data sheet
-   
-   %  ACCg = 2 * (rawData(k).acc - min(rawData(bestCalFit).acc) ) / ...
-   %  (max(rawData(bestCalFit).acc) - min(rawData(bestCalFit).acc) ) - 1;
-   
-   % Using Cmin and CMax from leaving the ACC on desk.
-   ACCg = ( (rawData(k).acc - CMin ) / (CMax - CMin) ) * 2 - 1;
-   
-    Fs = dataSet(k).header.samplingrate;   % Sampling frequency                    
-    T = 1/Fs;                              % Sampling period       
-    L = length(dataSet(k).data);           % Length of signal
-    f = Fs*(0:(L/2))/L;  
-   
-    Y = fft(ACCg);
-    Z = conj(Y);
-    X = abs(Y.*Z)/L;
-    freq = linspace(0,Fs,length(X));
-     
-   subplot(numOfFiles,plotSpacing,k)
-   plot(dataSet(k).t,ACCg)
-   
-   title(labels(1,k))
-   xlabel('Time in sec'); ylabel('g, (9.8 m/s^2)');
-   ylim([-3 3])
-   
-   grid on;
-   ax = gca;
-   ax.XRuler.MinorTick = 'on';
-
-end
-
-sgtitle('ACC g normalised vs time')
-
-
 % Testing power spectra calculation.
 %
 
-figure(3)
+figure(2)
 
 for k = 1:numOfFiles
 
     Fs = dataSet(k).header.samplingrate;
     L = length(dataSet(k).data);
-     % Using Cmin and CMax from leaving the ACC on desk.
-   ACCg = ( (rawData(k).acc - CMin ) / (CMax - CMin) ) * 2 - 1;
     
     Y = fft(rawData(k).acc);
     Z = conj(Y);
@@ -153,9 +101,9 @@ for k = 1:numOfFiles
     Fs = dataSet(k).header.samplingrate;
     L = length(dataSet(k).data);
 
-    Y = fft(rawData(k).acc)/L;
+    Y = fft(rawData(k).acc);
     Z = conj(Y);
-    X = abs(Y.*Z);
+    X = abs(Y.*Z)/L;
     freq = linspace(0,Fs,length(X));
     
     subplot(numOfFiles,plotSpacing,k)
